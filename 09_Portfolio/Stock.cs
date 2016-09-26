5770ff86a1b214f21b917f7101c9a708061500cd
kasp3939 <kasp3939@edu.eal.dk>
2016-09-26 13:56:58 +0200

    asd

--- 09_Portfolio/Stock.cs
+++ 09_Portfolio/Stock.cs
@@ -1,30 +1,85 @@
-﻿namespace _09_Portfolio
+﻿using System;
+
+namespace _09_Portfolio
 {
     internal class Stock
     {
+        private object obj;
+
         public Stock()
         {
 
         }
-        
+
+        public Stock(string v1, double v2, int v3)
+        {
+            Symbol = v1;
+            PricePerShare = v2;
+            NumShares = v3;
+
+        }
+
+        public Stock(object obj)
+        {
+            this.obj = obj;
+        }
+
+        public double NumShares
+        {
+            get;
+            internal set;
+        }
         public double PricePerShare
 
         {
-            get { return this.PricePerShare; }
-            internal set { this.PricePerShare = value; }
+            get;
+            internal set; 
         }
 
         public string Symbol
         {
-            get { return this.Symbol;  }
-            internal set { this.Symbol = value; }
-
-            
-            
+            get;
+            internal set;                     
         }
 
+        internal double GetValue()
+        {
+            //Convert.ToInt32(PricePerShare);
+            //Convert.ToDouble(NumShares);
+            //double getvalue = NumShares * PricePerShare;
+            //return getvalue;
+            return NumShares * PricePerShare;
+        }
 
+        internal static double TotalValue(Stock[] stocks)
+        {
+            //Stock[] Labels;
+            //Labels = new Stock[2];
+            //return Labels[];
+            double sum = 0;
 
-        
+            foreach 
+                (Stock stock in stocks)
+                    { sum += stock.GetValue(); }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            return "Stock[symbol=" + Symbol + ",pricePerShare=" + PricePerShare
+                + ",numShares=" + NumShares + "]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Stock s = (Stock)obj;
+
+            return (
+                (Symbol == s.Symbol) &&
+                (PricePerShare == s.PricePerShare) &&
+                (NumShares == s.NumShares));
+        }
+
     }
 }   
